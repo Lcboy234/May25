3fda9ff5617f18e4bbb87dc8b1b623b22abe91df
Kai <kaixiang980117@gmail.com>
2025-05-04 04:29:21 +0100

    final update for the day

---
+++ Day1/start.py
@@ -0,0 +1,17 @@
+# print("Hello " + input("What is your name?") + "!")
+
+# print(len(input("What is your name?")))
+
+# username = input("What is your name?")
+# length = len(username)
+# print(length)
+
+print("Welcome to the Band Name Generator.")
+
+a = input("What's the name of the city you grew up in?")
+print(a)
+
+b = input("What's your pet name?")
+print(b)
+
+print("Your band name could be " + a + " " + b)
