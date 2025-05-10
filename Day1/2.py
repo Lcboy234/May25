2ad469bc7700dd8e803297446a81c20f2e2425df
Kai <kaixiang980117@gmail.com>
2025-05-11 00:00:22 +0100

    final update for day 2

--- Day1/2.py
+++ Day1/2.py
@@ -1,13 +1,13 @@
 print("Welcome to the tip calculator!")
 
-Total = input("What was the total bill? £")
+Total = float(input("What was the total bill? £"))
 
-Tip = input("How much tip would you like to give? 10, 12, or 15? ")
+Tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 
-People = input("How many people to split the bill?")
+People = int(input("How many people to split the bill? "))
 
-Total_With_Tip = float(Total) + (float(Total) * int(Tip)/ 100)
+Total_With_Tip = Total + (Total * Tip/ 100)
 
-Final = round(Total_With_Tip/ int(People), 2)
+Final = round(Total_With_Tip/ People, 2)
 
 print(f"Each person should pay: £{Final}")
