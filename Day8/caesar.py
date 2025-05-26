23a025c942b4e190ea4aa231dbbf77341761728a
Kai <kaixiang980117@gmail.com>
2025-05-26 01:05:13 +0100

    final update to caesar

--- Day8/caesar.py
+++ Day8/caesar.py
@@ -1,18 +1,23 @@
 # TODO-1: Import and print the logo from art.py when the program starts.
 
-import 
+import art
+
+print(art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # TODO-2: What happens if the user enters a number/symbol/space?
 
-def caesar(original_text, shift_amount):
+def caesar(original_text, shift_amount, encode_or_decode):
     # set an empty string for ciphertext
     final_text = ""
 
     # check every letter for inputted string
     for letter in original_text:
-        if direction == "encode":
+        if letter not in alphabet:
+            final_text += letter
+
+        if encode_or_decode == "encode":
             # for example: alphabet = ['a', 'b', 'c', 'd', 'e']
             # using alphabet.index(letter) can check current position of letter
             # for example: alphabet.index("a") = 0
@@ -34,7 +39,7 @@ def caesar(original_text, shift_amount):
             # alphabet[] to find the actual string but not index() to find the position(int) of it
             final_text += alphabet[shifted_forward_position]
 
-        if direction == "decode":
+        if encode_or_decode == "decode":
             shifted_backward_position = alphabet.index(letter) - shift_amount
             shifted_backward_position %= len(alphabet)
             final_text += alphabet[shifted_backward_position]
@@ -43,8 +48,18 @@ def caesar(original_text, shift_amount):
 
 # TODO-3: Can you figure out a way to restart the cipher program?
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+game_over = False
 
-caesar(text, shift)
+while not game_over:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(text, shift, direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+
+    if restart == "no":
+        game_over = True
+        print("Goodbye")
