888cdf7a832caf5a6d2748a0342804f91f66f3a4
Kai <kaixiang980117@gmail.com>
2025-08-08 02:39:31 +0100

    update on OOP coffee maker

--- Day16/main.py
+++ Day16/main.py
@@ -1,25 +1,26 @@
-# from turtle import Turtle, Screen
+# # from turtle import Turtle, Screen
 
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("red")
-# timmy.forward
+# # timmy = Turtle()
+# # print(timmy)
+# # timmy.shape("turtle")
+# # timmy.color("red")
+# # timmy.forward
 
-# my_screen = Screen()
+# # my_screen = Screen()
 
-# # this is object attributes(variable)
-# print(my_screen.canvheight)
-# # this is methods(function)
-# my_screen.exitonclick()
+# # # this is object attributes(variable)
+# # print(my_screen.canvheight)
+# # # this is methods(function)
+# # my_screen.exitonclick()
 
-from prettytable import PrettyTable
-table = PrettyTable()
+# from prettytable import PrettyTable
+# table = PrettyTable()
 
-# this is methods
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
+# # this is methods
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
 
-# this is attributes
-table.align = "l"
-print(table)
+# # this is attributes
+# table.align = "l"
+# print(table)
+
