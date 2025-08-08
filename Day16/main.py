68d34ef5696866ca6fcae772e769974f715a43a2
Kai <kaixiang980117@gmail.com>
2025-08-08 02:05:42 +0100

    test run

---
+++ Day16/main.py
@@ -0,0 +1,17 @@
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward
+
+# my_screen = Screen()
+
+# # this is object attributes(variable)
+# print(my_screen.canvheight)
+# # this is methods(function)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
