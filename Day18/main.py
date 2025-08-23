848e298d18bd20ec6d052d44edd669b5eee837c9
Kai <kaixiang980117@gmail.com>
2025-08-23 04:25:17 +0100

    day 18

--- Day18/main.py
+++ Day18/main.py
@@ -9,15 +9,27 @@
 # screen = Screen()
 # screen.exitonclick()
 
+
+# for i in range(8):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
 from turtle import *
 
 tim = Turtle()
 
-for i in range(8):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+def draw(num_sides):
+
+    angle = 360/ num_sides
+    
+    for i in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape in range(3, 11):
+    draw(shape)
 
 screen = Screen()
 screen.exitonclick()
