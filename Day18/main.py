50c901296f5941805071ae3d81a6bb941e053431
Kai <kaixiang980117@gmail.com>
2025-08-23 04:15:42 +0100

    fixed venv access and completed dashed

--- Day18/main.py
+++ Day18/main.py
@@ -1,10 +1,23 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
+# timmy_the_turtle = Turtle()
 
-for i in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+# for i in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+
+# screen = Screen()
+# screen.exitonclick()
+
+from turtle import *
+
+tim = Turtle()
+
+for i in range(8):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
 screen = Screen()
 screen.exitonclick()
